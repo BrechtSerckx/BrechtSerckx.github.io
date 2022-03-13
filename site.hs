99b925cea621e6e8e34bf7a2a7aaca27428b5e12
Brecht Serckx <services+git@brechtserckx.be>
2022-03-13 18:32:22 +0100

    Custom main function to rebuild with ghcid

--- site.hs
+++ site.hs
@@ -3,7 +3,16 @@ module Main where
 import           Hakyll
 
 main :: IO ()
-main = hakyll $ do
+main = hakyll siteRules
+
+mainBuild :: IO ()
+mainBuild =
+  let configuration = defaultConfiguration
+      options       = Options False Build
+  in  hakyllWithArgs configuration options siteRules
+
+siteRules :: Rules ()
+siteRules = do
   match "images/*" $ do
     route idRoute
     compile copyFileCompiler
