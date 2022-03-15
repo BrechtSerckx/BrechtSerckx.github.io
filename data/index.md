1191206f471730b17a52c89b29d0094ed4db752e
Brecht Serckx <services+git@brechtserckx.be>
2022-03-15 12:56:46 +0100

    Add homepage

---
+++ data/index.md
@@ -0,0 +1,7 @@
+---
+title: Home welcome
+subtitle: Welcome to my website
+image: /images/haskell-logo.png
+---
+
+Welcome to my interactive curriculum vitae, also called a *website*.
--- site.hs
+++ site.hs
@@ -21,4 +21,31 @@ siteRules = do
     route idRoute
     compile compressCssCompiler
 
+  match "data/**.md" $ compile pandocCompiler
+
+  create ["index.html"] $ do
+    route idRoute
+    compile $ do
+      let bodyCtx     = field "body" $ \_ -> loadBody "data/index.md"
+          metadataCtx = metadataFieldFrom "data/index.md"
+          templateCtx = metadataFieldFrom "templates/index.html"
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/index.html"
+                                 (bodyCtx <> metadataCtx <> defaultContext)
+        >>= loadAndApplyTemplate
+              "templatesDefault.html"
+              (templateCtx <> metadataCtx <> defaultContext)
+        >>= relativizeUrls
+
   match "templates/*" $ compile templateBodyCompiler
+
+metadataFieldFrom :: Identifier -> Context a
+metadataFieldFrom id' = Context $ \k _ _i -> do
+  let empty' =
+        noResult
+          $  "No '"
+          ++ k
+          ++ "' field in metadata "
+          ++ "of items "
+          ++ show id'
+  maybe empty' (return . StringField) =<< getMetadataField id' k
